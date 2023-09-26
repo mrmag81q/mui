01ae52bea3b2f649e981bb7a85f5e382fe8e2540
mrmag81q <mrmag1728@gmail.com>
2023-09-26 23:29:09 +0330

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button'
+ 
+
 
 function App() {
+  const UseStyle = styled(Button)(({theme})=>({
+      backgroundColor : theme.palette.primary.main,
+      color : "white"
+    }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <UseStyle variant="contained" >
+        s
+      </UseStyle>
+    
     </div>
   );
 }
